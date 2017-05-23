24548c2789ba0a5df436bef2c9cfc2e13b5f8fb0
ksmai <kasingmai@gmail.com>
2017-05-23 21:53:31 +0800

    Update PlayerInfo component

--- src/app/player/player-info/player-info.component.ts
+++ src/app/player/player-info/player-info.component.ts
@@ -1,12 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
 
-import { Track } from '../../../data-models/track';
+import { Playing } from '../../../data-models/playing';
+import { PlayerService } from '../../core/player.service';
 
 @Component({
   selector: 'spot-player-info',
   templateUrl: './player-info.component.html',
   styleUrls: ['./player-info.component.scss'],
 })
-export class PlayerInfoComponent {
-  @Input() track: Track;
+export class PlayerInfoComponent implements OnInit {
+  currentStatus: Observable<Playing>;
+
+  constructor(private playerService: PlayerService) {
+  }
+
+  ngOnInit() {
+    this.currentStatus = this.playerService
+      .getCurrentStatus()
+      .map((status: Playing) => status.track ? status : null);
+  }
 }
