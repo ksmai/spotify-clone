a096256ac6bda74664f342066bd1956a593b2673
ksmai <kasingmai@gmail.com>
2017-05-26 22:32:53 +0800

    Add tests for AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,39 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+
+import { FakeRouter } from '../test-utils/';
+import { AppComponent } from './app.component';
+
+let fixture: ComponentFixture<AppComponent>;
+let component: AppComponent;
+
+function createComponent() {
+  fixture = TestBed.createComponent(AppComponent);
+  component = fixture.componentInstance;
+}
+
 describe('AppComponent', () => {
-  it('passes', () => 0);
+  beforeEach(async(() => {
+    TestBed
+      .configureTestingModule({
+        declarations: [
+          AppComponent,
+        ],
+        providers: [
+          { provide: Router, useClass: FakeRouter },
+        ],
+        schemas: [
+          NO_ERRORS_SCHEMA,
+        ],
+      })
+      .compileComponents()
+      .then(() => createComponent());
+  }));
+
+  it('should scroll after navigation', () => {
+    const scrollSpy = spyOn(window, 'scrollTo');
+    component.ngOnInit();
+    expect(scrollSpy).toHaveBeenCalled();
+  });
 });
--- src/test-utils/fake-router.ts
+++ src/test-utils/fake-router.ts
@@ -1,7 +1,11 @@
 /* tslint:disable no-empty */
 import { Injectable } from '@angular/core';
+import { NavigationEnd } from '@angular/router';
+import 'rxjs/add/observable/of';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class FakeRouter {
+  events = Observable.of(new NavigationEnd(1, '/', '/'));
   navigate(url: any) { }
 }
